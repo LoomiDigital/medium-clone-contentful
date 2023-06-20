55220ebecc1f863612ada2179f401c8da4612152
Iñigo Villalba <inigo@loomi.io>
2023-06-20 19:35:34 +0200

    Add ssr revalidate url for webhook

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  console.log("Revalidating");
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+
+    const reqBody = JSON.parse(req.body);
+
+    const slug = reqBody.fields.slug["en-US"];
+
+    await res.revalidate(`/post/${slug}`);
+
+    console.log("Revalidated");
+    return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating");
+  }
+}
